f177f9bc5c84fefdf904d4c200557b756a1ec766
psyomn <lethaljellybean@gmail.com>
2019-06-17 19:16:05 -0400

    Change game name

--- blind-archer/conf.lua
+++ blind-archer/conf.lua
@@ -6,6 +6,6 @@ function love.conf(t)
    t.version = "11.2"
    t.window.width = s.window.x
    t.window.height = s.window.y
-   t.window.title = "blind archer"
+   t.window.title = "FAIM"
    t.window.fullscreen = false
 end
