da05af034c50b9d78b9346ebf99145b436f26c9a
psyomn <lethaljellybean@gmail.com>
2019-06-17 18:43:31 -0400

    Fix window borders

--- blind-archer/main.lua
+++ blind-archer/main.lua
@@ -17,16 +17,40 @@ function love.load()
    -- theme:setLooping(true)
    -- theme:play()
 
-   -- Physics
-   love.physics.setMeter(16)
-   world = love.physics.newWorld(0, 9.81 * 64, true)
-   world:setCallbacks(beginContact, endContact, preSolve, postSolve)
-
    -- Scaling
    windowWidth = love.graphics.getWidth()
    windowHeight = love.graphics.getHeight()
    xscale = windowWidth / sett.window.x
    yscale = windowHeight / sett.window.y
+
+   -- Physics
+   love.physics.setMeter(16)
+   world = love.physics.newWorld(0, 9.81 * 64, true)
+   world:setCallbacks(beginContact, endContact, preSolve, postSolve)
+
+     -- Window borders
+     borderWidth = 16
+     windowBorders = {}
+     windowBorders.left = {}
+     windowBorders.right = {}
+     windowBorders.top = {}
+     windowBorders.bottom = {}
+
+     local py = love.physics
+     windowBorders.left.body = py.newBody(world, -16, 0)
+     windowBorders.left.shape = py.newRectangleShape(borderWidth, windowHeight * 2)
+     windowBorders.left.fixture = py.newFixture(windowBorders.left.body, windowBorders.left.shape, 1)
+     windowBorders.left.fixture:setFilterData(CATEGORY_SCENERY, FULL_MASK, GROUP_WALL)
+
+     windowBorders.right.body = py.newBody(world, windowWidth, 0)
+     windowBorders.right.shape = py.newRectangleShape(borderWidth, windowHeight * 2)
+     windowBorders.right.fixture = py.newFixture(windowBorders.right.body, windowBorders.right.shape, 1)
+     windowBorders.right.fixture:setFilterData(CATEGORY_SCENERY, FULL_MASK, GROUP_WALL)
+
+     windowBorders.top.body = py.newBody(world, 0, 0)
+     windowBorders.top.shape = py.newRectangleShape(windowWidth, borderWidth)
+     windowBorders.top.fixture = py.newFixture(windowBorders.top.body, windowBorders.top.shape, 1)
+     windowBorders.top.fixture:setFilterData(CATEGORY_SCENERY, FULL_MASK, GROUP_WALL)
 
    -- Actors
    ch = Archer:new(10, 10)
@@ -35,8 +59,6 @@ function love.load()
 
    ch:couple(bu)
    bu:couple(ch)
-
-   e1 = Enemy.makeGnome(30, 10)
 
    -- Level
    level = Level:new()
